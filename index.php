f54efd940862e9d462d210189f706069996f3911
Nguyen Louis <nguyenlouis172@gmail.com>
2022-08-23 08:35:54 +0200

    add Form

--- index.php
+++ index.php
@@ -8,24 +8,22 @@ $params = explode ("/", $_GET["p"]);
 print_r($params);
 echo ROOT;
 */
+/*
 require "Personnage.php";
 $cesar= new Personnage("Cesar");
-/*
+
 $cesar->regenerer(5);
 $cesar->nom="Cesar"; remplace by the constructor
-*/
+
 
 $octave= new Personnage("Octave");
-/*
 $cesar->attaque($octave);
 if($octave->mort()){
     echo "Octave est mort";
 }else {
     echo "Octave a survecu avec " . $octave->vie;
 }
-*/
 
-/*
 $octave->vie=0;
 var_dump($octave->mort());
 $octave->regenerer();
@@ -34,11 +32,32 @@ var_dump($cesar->atk);
 var_dump($cesar->crier());
 var_dump($cesar);
 var_dump($octave);
-*/
+
 $cesar->setNom ("Caesar");
 echo $cesar->getVie();
 echo $cesar->getAtk();
 echo $cesar->getNom();// access Nom  if private class
-
-
+*/
+require "form.php";
+$form= new Form($_POST);
 ?>
+/*
+$form = new Form(array("username"=>"Roger"));
+var_dump($form);
+die();
+echo $form->input ("username");
+echo $form->input("password");
+echo $form->submit();
+
+$form = new Form();
+echo $form->input ("age");
+echo $form->input ("sexe");
+*/
+
+<form action="#" method="post">
+    <?php
+    echo $form=input("username");
+    echo $form=input("password");
+    echo $form=inputsubmit();
+?>
+</form>
